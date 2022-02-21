c2ff6e067fc72b18df0b2da93b276db13a6dc269
MainCode Inc <99421749+MainCodeInc@users.noreply.github.com>
2022-02-21 19:39:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-#This Algo is still being built!
+#This Algorithm takes user info in python and re-displays it,
+#This Algo is still being built,
 #Pls be patient...This algo will be published soon!
