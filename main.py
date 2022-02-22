976eb6a07970e0100ed5b83a9183e57f74359271
MainCode Inc <99421749+MainCodeInc@users.noreply.github.com>
2022-02-22 16:19:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-#This Algorithm takes user info in python and re-displays it,
-#This Algo is still being built,
-#Pls be patient...This algo will be published soon!
+#AI details program
+
+import time
+
+space = ("---------------------------------------------")
+
+print("Hello and welcome to the AI details program!")
+time.sleep(1.5)
+print(space)
+time.sleep(3)
+
+firstname = input("Please enter your first name? ")
+time.sleep(1.5)
+surname = input("Please enter your surname? ")
+time.sleep(1.5)
+Age = input("Please enter your Age? ")
+time.sleep(1.5)
+Hobby = input("Please enter you hobby? ")
+time.sleep(1.5)
+Country = input("Please enter which country you are from? ")
+time.sleep(1.5)
+Birthdate = input("Please Enter you Birthdate? ")
+time.sleep(2)
+print(space)
+time.sleep(3)
+print("Loading info...")
+time.sleep(3)
+print("---------------")
+
+print("Hi", firstname, surname)
+time.sleep(2.5)
+print("Aged:", Age)
+time.sleep(2.5)
+print("He likes", Hobby)
+time.sleep(2.5)
+print("From", Country)
+time.sleep(2.5)
+print("Born in", Birthdate)
+time.sleep(2.5)
+
+#End of program
